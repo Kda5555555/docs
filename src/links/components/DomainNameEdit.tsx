2d542d5543034a0b19267ab9a00740c124c12a5e
Kda5555555 <166656690+Kda5555555@users.noreply.github.com>
2024-05-19 22:28:53 +0300

    Revert "Repo sync"

--- src/links/components/DomainNameEdit.tsx
+++ src/links/components/DomainNameEdit.tsx
@@ -6,13 +6,10 @@ import { useTranslation } from 'src/languages/components/useTranslation'
 import { Box, Flash, FormControl, Spinner, Text, TextInput } from '@primer/react'
 import { Dialog } from '@primer/react/experimental'
 import { useEditableDomainName } from './useEditableDomainContext'
-import { sendEvent, EventType } from 'src/events/components/events'
 
 type Props = {
   xs?: boolean
 }
-
-const EXPERIMENT_NAME = 'domain_edit'
 
 const QUERY_STRING_KEY = 'ghdomain' // Must match the middleware
 
@@ -46,13 +43,6 @@ export default function DomainNameEdit({ xs }: Props) {
             target.parentElement.parentElement.classList.contains('replacedomain-edit'))
         ) {
           setOpen(true)
-
-          sendEvent({
-            type: EventType.experiment,
-            experiment_name: EXPERIMENT_NAME,
-            experiment_variation: 'opened',
-            experiment_success: true,
-          })
         }
       }
     }
@@ -65,17 +55,6 @@ export default function DomainNameEdit({ xs }: Props) {
       if (main) {
         main.removeEventListener('click', handler)
       }
-    }
-  }, [asPath])
-
-  useEffect(() => {
-    if (document.querySelectorAll('code[data-replacedomain]').length > 0) {
-      sendEvent({
-        type: EventType.experiment,
-        experiment_name: EXPERIMENT_NAME,
-        experiment_variation: 'available',
-        experiment_success: true,
-      })
     }
   }, [asPath])
 
@@ -103,13 +82,6 @@ export default function DomainNameEdit({ xs }: Props) {
         } else {
           setSubmissionFailed(true)
         }
-
-        sendEvent({
-          type: EventType.experiment,
-          experiment_name: EXPERIMENT_NAME,
-          experiment_variation: 'saved',
-          experiment_success: true,
-        })
       })
       .finally(() => {
         setLoading(false)
@@ -149,16 +121,7 @@ export default function DomainNameEdit({ xs }: Props) {
               disabled: !!validationError || loading,
             },
           ]}
-          onClose={() => {
-            setOpen(false)
-
-            sendEvent({
-              type: EventType.experiment,
-              experiment_name: EXPERIMENT_NAME,
-              experiment_variation: 'closed',
-              experiment_success: true,
-            })
-          }}
+          onClose={() => setOpen(false)}
           aria-labelledby="header"
         >
           <form
